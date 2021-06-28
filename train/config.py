7c0a1c24736838648ad9f2ad454d298fbcd5d6e9
kevinbdsouza <kdsouza1496@gmail.com>
2021-06-28 16:47:27 -0700

    config

--- train/config.py
+++ train/config.py
@@ -27,7 +27,7 @@ class Config:
             "L": 10,
             "n_heads": 8,
             "hidden_dim": 16,
-            "out_dim": 2,
+            "out_dim": 8,
             "edge_feat": True,
             "residual": True,
             "readout": "mean",
