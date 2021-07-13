e03ce3937397e4bb1af14d5b18e169fd39369583
kevinbdsouza <kdsouza1496@gmail.com>
2021-07-13 16:21:09 -0700

    update

--- train/config.py
+++ train/config.py
@@ -24,7 +24,7 @@ class Config:
         ##########################################
 
         self.net_params = {
-            "L": 10,
+            "L": 2,
             "n_heads": 8,
             "hidden_dim": 16,
             "out_dim": 8,
